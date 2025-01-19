c360ec3e1e8bcdad83a83e733e3fca8dacb67755
Mandeep Singh <mandeepsingh@Mandeeps-MacBook-Air.local>
2025-01-19 14:32:37 +0530

    script update

--- script/backup.sh
+++ script/backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SUFFIX=$(date '+%Y-%m-%d-%H:%M:%S')
-
-mkdir backup
-cp data/teams.csv backup/teams-$SUFFIX.csv
-cp data/alpine0.csv backup/alpine0-$SUFFIX.csv
+while true
+do
+    SUFFIX=$(date '+%Y-%m-%d-%H:%M:%S')
+    cp data/teams.csv backup/teams-$SUFFIX.csv
+    cp data/apl0.csv backup/apl0-$SUFFIX.csv
+    sleep 60
+done
